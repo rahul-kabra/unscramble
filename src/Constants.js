cf1575e6ce3ad5c68b491c009a4ad17e0e59a69f
Rahul Kabra <rahulkabra029@gmail.com>
2020-09-09 22:53:40 +0530

    added level 1 words

---
+++ src/Constants.js
@@ -0,0 +1,102 @@
+export const LEVEL_1 = [
+  {
+    actual: "please",
+    scrambled: "lepeas"
+  },
+  {
+    actual: "courtesy",
+    scrambled: "cosetryu"
+  },
+  {
+    actual: "apple",
+    scrambled: "pepla"
+  },
+  {
+    actual: "tiger",
+    scrambled: "retgi"
+  },
+  {
+    actual: "cream",
+    scrambled: "macer"
+  },
+  {
+    actual: "matter",
+    scrambled: "tatrem"
+  },
+  {
+    actual: "snowy",
+    scrambled: "ywnso"
+  },
+  {
+    actual: "chocolate",
+    scrambled: "chaceloto"
+  },
+  {
+    actual: "kicked",
+    scrambled: "deckki"
+  },
+  {
+    actual: "believe",
+    scrambled: "eveelbi"
+  },
+  {
+    actual: "storm",
+    scrambled: "morts"
+  },
+  {
+    actual: "morning",
+    scrambled: "mingnor"
+  },
+  {
+    actual: "noisy",
+    scrambled: "ysoni"
+  },
+  {
+    actual: "potato",
+    scrambled: "taotop"
+  },
+  {
+    actual: "seconds",
+    scrambled: "condess"
+  },
+  {
+    actual: "summer",
+    scrambled: "umrmes"
+  },
+  {
+    actual: "funny",
+    scrambled: "nyunf"
+  },
+  {
+    actual: "elephant",
+    scrambled: "thanelep"
+  },
+  {
+    actual: "jasmine",
+    scrambled: "jinsame"
+  },
+  {
+    actual: "wheat",
+    scrambled: "wetah"
+  },
+  {
+    actual: "cheese",
+    scrambled: "seeech"
+  },
+  {
+    actual: "faith",
+    scrambled: "ifhat"
+  },
+  {
+    actual: "beautiful",
+    scrambled: "faibleutu"
+  },
+  {
+    actual: "princess",
+    scrambled: "crispens"
+  },
+  {
+    actual: "knight",
+    scrambled: "giknth"
+  }
+];
