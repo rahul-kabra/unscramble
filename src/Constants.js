ad89e6506c3c922976f32f99b568de8523e2cc1f
Rahul Kabra <rahulkabra029@gmail.com>
2020-09-10 23:18:05 +0530

    added level 2 words

--- src/Constants.js
+++ src/Constants.js
@@ -100,3 +100,105 @@ export const LEVEL_1 = [
     scrambled: "giknth"
   }
 ];
+export const LEVEL_2 = [
+  {
+    actual: "football",
+    scrambled: "tolaflob"
+  },
+  {
+    actual: "music",
+    scrambled: "sucmi"
+  },
+  {
+    actual: "school",
+    scrambled: "loshoc"
+  },
+  {
+    actual: "zebra",
+    scrambled: "braze"
+  },
+  {
+    actual: "water",
+    scrambled: "tawer"
+  },
+  {
+    actual: "minutes",
+    scrambled: "mistune"
+  },
+  {
+    actual: "mushroom",
+    scrambled: "humsmoor"
+  },
+  {
+    actual: "candy",
+    scrambled: "dancy"
+  },
+  {
+    actual: "swimming",
+    scrambled: "miwingsm"
+  },
+  {
+    actual: "afternoon",
+    scrambled: "nonfatore"
+  },
+  {
+    actual: "lyrics",
+    scrambled: "clysri"
+  },
+  {
+    actual: "cycle",
+    scrambled: "leccy"
+  },
+  {
+    actual: "building",
+    scrambled: "unlidgib"
+  },
+  {
+    actual: "bottle",
+    scrambled: "teltob"
+  },
+  {
+    actual: "sunshine",
+    scrambled: "neshsuni"
+  },
+  {
+    actual: "vacation",
+    scrambled: "cavaoint"
+  },
+  {
+    actual: "holiday",
+    scrambled: "hyoidal"
+  },
+  {
+    actual: "computer",
+    scrambled: "courempt",
+  },
+  {
+    actual: "chair",
+    scrambled: "archi",
+  },
+  {
+    actual: "sports",
+    scrambled: "orspst",
+  },
+  {
+    actual: "magic",
+    scrambled: "gamic",
+  },
+  {
+    actual: "thank",
+    scrambled: "tkanh"
+  },
+  {
+    actual: "remind",
+    scrambled: "nidrem"
+  },
+  {
+    actual: "giraffe",
+    scrambled: "rifegaf"
+  },
+  {
+    actual: "stars",
+    scrambled: "tsars"
+  }
+];
