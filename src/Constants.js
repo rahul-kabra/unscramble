27300c183069594aa659107743effca4307d04c2
Rahul Kabra <rahulkabra029@gmail.com>
2020-09-11 20:34:49 +0530

    added level 3 words. also added clues.

--- src/Constants.js
+++ src/Constants.js
@@ -1,204 +1,381 @@
 export const LEVEL_1 = [
   {
     actual: "please",
-    scrambled: "lepeas"
+    scrambled: "lepeas",
+    clue: "Cause to feel happy and satisfied.",
   },
   {
     actual: "courtesy",
-    scrambled: "cosetryu"
+    scrambled: "cosetryu",
+    clue: "Polite behaviour, or a polite action or remark.",
   },
   {
     actual: "apple",
-    scrambled: "pepla"
+    scrambled: "pepla",
+    clue: "This fruit fell on Newton's head.",
   },
   {
     actual: "tiger",
-    scrambled: "retgi"
+    scrambled: "retgi",
+    clue: "Burning bright, in the forests of the night.",
   },
   {
     actual: "cream",
-    scrambled: "macer"
+    scrambled: "macer",
+    clue: "Fatty part of milk.",
   },
   {
     actual: "matter",
-    scrambled: "tatrem"
+    scrambled: "tatrem",
+    clue: "Anything that has mass and volume.",
   },
   {
     actual: "snowy",
-    scrambled: "ywnso"
+    scrambled: "ywnso",
+    clue: "Winters are generally...",
   },
   {
     actual: "chocolate",
-    scrambled: "chaceloto"
+    scrambled: "chaceloto",
+    clue: "Made from roasted and ground cacao seeds.",
   },
   {
     actual: "kicked",
-    scrambled: "deckki"
+    scrambled: "deckki",
+    clue: "Hit forcibly with the foot.",
   },
   {
     actual: "believe",
-    scrambled: "eveelbi"
+    scrambled: "eveelbi",
+    clue: "Accept that something is true, especially without proof.",
   },
   {
     actual: "storm",
-    scrambled: "morts"
+    scrambled: "morts",
+    clue: "A violent disturbance of the atmosphere with strong winds and usually rain, thunder, lightning, or snow.",
   },
   {
     actual: "morning",
-    scrambled: "mingnor"
+    scrambled: "mingnor",
+    clue: "The period of time between midnight and noon, especially from sunrise to noon.",
   },
   {
     actual: "noisy",
-    scrambled: "ysoni"
+    scrambled: "ysoni",
+    clue: "Making a lot of noise.",
   },
   {
     actual: "potato",
-    scrambled: "taotop"
+    scrambled: "taotop",
+    clue: "Couch what?",
   },
   {
     actual: "seconds",
-    scrambled: "condess"
+    scrambled: "condess",
+    clue: "Base unit of time in the International System of Units.",
   },
   {
     actual: "summer",
-    scrambled: "umrmes"
+    scrambled: "umrmes",
+    clue: "Warmest season of the year.",
   },
   {
     actual: "funny",
-    scrambled: "nyunf"
+    scrambled: "nyunf",
+    clue: "Humorous.",
   },
   {
     actual: "elephant",
-    scrambled: "thanelep"
+    scrambled: "thanelep",
+    clue: "Largest land animal.",
   },
   {
     actual: "jasmine",
-    scrambled: "jinsame"
+    scrambled: "jinsame",
+    clue: "Flower famous for its scented oil and perfume.",
   },
   {
     actual: "wheat",
-    scrambled: "wetah"
+    scrambled: "wetah",
+    clue: "A cereal grain which is a worldwide staple food.",
   },
   {
     actual: "cheese",
-    scrambled: "seeech"
+    scrambled: "seeech",
+    clue: "Cheddar, parmesan and mozzarella.",
   },
   {
     actual: "faith",
-    scrambled: "ifhat"
+    scrambled: "ifhat",
+    clue: "Complete trust or confidence in someone or something.",
   },
   {
     actual: "beautiful",
-    scrambled: "faibleutu"
+    scrambled: "faibleutu",
+    clue: "Pleasing the senses or mind aesthetically.",
   },
   {
     actual: "princess",
-    scrambled: "crispens"
+    scrambled: "crispens",
+    clue: "The daughter of a monarch.",
   },
   {
     actual: "knight",
-    scrambled: "giknth"
+    scrambled: "giknth",
+    clue: "A man who served his sovereign or lord as a mounted soldier in armour.",
   }
 ];
 export const LEVEL_2 = [
   {
     actual: "football",
-    scrambled: "tolaflob"
+    scrambled: "tolaflob",
+    clue: "World's most popular sport.",
   },
   {
     actual: "music",
-    scrambled: "sucmi"
+    scrambled: "sucmi",
+    clue: "An art form, and cultural activity, whose medium is sound.",
   },
   {
     actual: "school",
-    scrambled: "loshoc"
+    scrambled: "loshoc",
+    clue: "An institution for educating children.",
   },
   {
     actual: "zebra",
-    scrambled: "braze"
+    scrambled: "braze",
+    clue: "An African wild horse with black-and-white stripes.",
   },
   {
     actual: "water",
-    scrambled: "tawer"
+    scrambled: "tawer",
+    clue: "3/4 th of Earth is covered by this.",
   },
   {
     actual: "minutes",
-    scrambled: "mistune"
+    scrambled: "mistune",
+    clue: "A period of time equal to sixty seconds.",
   },
   {
     actual: "mushroom",
-    scrambled: "humsmoor"
+    scrambled: "humsmoor",
+    clue: "Conspicuous umbrella-shaped fruiting body (sporophore) of certain fungi that you can eat.",
   },
   {
     actual: "candy",
-    scrambled: "dancy"
+    scrambled: "dancy",
+    clue: "Confectionery.",
   },
   {
     actual: "swimming",
-    scrambled: "miwingsm"
+    scrambled: "miwingsm",
+    clue: "The sport or activity of propelling oneself through water using the limbs.",
   },
   {
     actual: "afternoon",
-    scrambled: "nonfatore"
+    scrambled: "nonfatore",
+    clue: "The time from noon or lunchtime to evening.",
   },
   {
     actual: "lyrics",
-    scrambled: "clysri"
+    scrambled: "clysri",
+    clue: "The words of a song.",
   },
   {
     actual: "cycle",
-    scrambled: "leccy"
+    scrambled: "leccy",
+    clue: "A series of events that are regularly repeated in the same order.",
   },
   {
     actual: "building",
-    scrambled: "unlidgib"
+    scrambled: "unlidgib",
+    clue: "A structure with a roof and walls, such as a house or factory.",
   },
   {
     actual: "bottle",
-    scrambled: "teltob"
+    scrambled: "teltob",
+    clue: "A glass or plastic container, used for storing drinks or other liquids.",
   },
   {
     actual: "sunshine",
-    scrambled: "neshsuni"
+    scrambled: "neshsuni",
+    clue: "Sunlight.",
   },
   {
     actual: "vacation",
-    scrambled: "cavaoint"
+    scrambled: "cavaoint",
+    clue: "Holiday period.",
   },
   {
     actual: "holiday",
-    scrambled: "hyoidal"
+    scrambled: "hyoidal",
+    clue: "An extended period of leisure and recreation, especially one spent away from home or in travelling.",
   },
   {
     actual: "computer",
     scrambled: "courempt",
+    clue: "An electronic device for storing and processing data, typically in binary form.",
   },
   {
     actual: "chair",
     scrambled: "archi",
+    clue: "A seat typically having four legs and a back for one person.",
   },
   {
     actual: "sports",
     scrambled: "orspst",
+    clue: "An athletic activity requiring skill or physical prowess and often of a competitive nature.",
   },
   {
     actual: "magic",
     scrambled: "gamic",
+    clue: "The power of apparently influencing events by using mysterious or supernatural forces.",
   },
   {
     actual: "thank",
-    scrambled: "tkanh"
+    scrambled: "tkanh",
+    clue: "Express gratitude to someone.",
   },
   {
     actual: "remind",
-    scrambled: "nidrem"
+    scrambled: "nidrem",
+    clue: "Cause (someone) to remember someone or something.",
   },
   {
     actual: "giraffe",
-    scrambled: "rifegaf"
+    scrambled: "rifegaf",
+    clue: "A large African mammal with a very long neck and forelegs.",
   },
   {
     actual: "stars",
-    scrambled: "tsars"
+    scrambled: "tsars",
+    clue: "A fixed luminous point in the night sky which is a large, remote incandescent body like the sun.",
+  }
+];
+export const LEVEL_3 = [
+  {
+    actual: "adventure",
+    scrambled: "detenuvar",
+    clue: "An unusual and exciting or daring experience.",
+  },
+  {
+    actual: "examination",
+    scrambled: "taximennaoi",
+    clue: "A detailed inspection or study.",
+  },
+  {
+    actual: "student",
+    scrambled: "nettuds",
+    clue: "A school or college pupil.",
+  },
+  {
+    actual: "employee",
+    scrambled: "peeoymel",
+    clue: "Someone who gets paid to work for a person or company.",
+  },
+  {
+    actual: "ground",
+    scrambled: "nugdor",
+    clue: "The solid surface of the Earth.",
+  },
+  {
+    actual: "tower",
+    scrambled: "owtre",
+    clue: "A tall, narrow building, that either stands alone or forms part of another building.",
+  },
+  {
+    actual: "kingdom",
+    scrambled: "dimgonk",
+    clue: "A country, state, or territory ruled by a king or queen.",
+  },
+  {
+    actual: "beverage",
+    scrambled: "grebevae",
+    clue: "A drink other than water.",
+  },
+  {
+    actual: "celebrate",
+    scrambled: "abletcree",
+    clue: "To express admiration and approval for something or someone.",
+  },
+  {
+    actual: "backyard",
+    scrambled: "drabyack",
+    clue: "A back garden.",
+  },
+  {
+    actual: "awesome",
+    scrambled: "owesmae",
+    clue: "Extremely impressive.",
+  },
+  {
+    actual: "invite",
+    scrambled: "vietin",
+    clue: "To request the presence or participation of.",
+  },
+  {
+    actual: "sorted",
+    scrambled: "rostde",
+    clue: "An exclamation of satisfaction.",
+  },
+  {
+    actual: "television",
+    scrambled: "envoislite",
+    clue: "An electronic device used to receive sound and images which create television programs that people watch.",
+  },
+  {
+    actual: "aeroplane",
+    scrambled: "reponeala",
+    clue: "A powered flying vehicle with fixed wings and a weight greater than that of the air it displaces.",
+  },
+  {
+    actual: "orange",
+    scrambled: "regoan",
+    clue: "A colour between yellow and red on the spectrum of visible light.",
+  },
+  {
+    actual: "monument",
+    scrambled: "munmoten",
+    clue: "A statue, building, or other structure erected to commemorate a notable person or event.",
+  },
+  {
+    actual: "mythology",
+    scrambled: "hootyglym",
+    clue: "The study of myths.",
+  },
+  {
+    actual: "stadium",
+    scrambled: "tuismad",
+    clue: "An athletic or sports ground with tiers of seats for spectators.",
+  },
+  {
+    actual: "arena",
+    scrambled: "reaan",
+    clue: "A place where sports, entertainments, and other public events take place.",
+  },
+  {
+    actual: "tournament",
+    scrambled: "moutantern",
+    clue: "A series of contests between a number of competitors, competing for an overall prize.",
+  },
+  {
+    actual: "lemur",
+    scrambled: "merlu",
+    clue: "An arboreal primate with a pointed snout and typically a long tail, found only in Madagascar.",
+  },
+  {
+    actual: "chemical",
+    scrambled: "chiaclem",
+    clue: "A substance produced by or used in a chemical process.",
+  },
+  {
+    actual: "factory",
+    scrambled: "fryatoc",
+    clue: "A building or group of buildings where goods are manufactured or assembled chiefly by machine.",
+  },
+  {
+    actual: "atmosphere",
+    scrambled: "espartomeh",
+    clue: "The envelope of gases surrounding the earth or another planet.",
   }
 ];
