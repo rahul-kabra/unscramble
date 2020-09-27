7868fec6135ffed77c22714cfe66cd85762b7499
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-27 11:53:40 +0000

    Deploy rahul-kabra/unscramble to github.com/rahul-kabra/unscramble.git:gh-pages

---
+++ precache-manifest.7076f5badb5e1f51e01f2f0f9f235697.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0649ff63e5cb3767eb1e939dbb0fab3",
+    "url": "/unscramble/index.html"
+  },
+  {
+    "revision": "507800fd962f281331be",
+    "url": "/unscramble/static/css/main.7fd53dba.chunk.css"
+  },
+  {
+    "revision": "124c613204a55dbe19db",
+    "url": "/unscramble/static/js/2.1e378027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/unscramble/static/js/2.1e378027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "507800fd962f281331be",
+    "url": "/unscramble/static/js/main.13f6e12d.chunk.js"
+  },
+  {
+    "revision": "8e97f2ff0d3da41347c1",
+    "url": "/unscramble/static/js/runtime-main.f2557d29.js"
+  }
+]);
